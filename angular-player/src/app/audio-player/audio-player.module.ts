1722103afac1f3e0ec53ea219d827f11b744ceb1
mosquerapaul <mosquera.paul@gmail.com>
2019-11-07 13:17:29 +0100

    AudioPlayer structure created: - PlayList to list audio files - PlayerControls to allow user play, stop and jump back and forward

--- angular-player/src/app/audio-player/audio-player.module.ts
+++ angular-player/src/app/audio-player/audio-player.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AudioPlayerComponent } from './audio-player/audio-player.component';
+import { PlayerControlsComponent } from './player-controls/player-controls.component';
+import { PlayListComponent } from './play-list/play-list.component';
 
 
 
 @NgModule({
-  declarations: [AudioPlayerComponent],
+  declarations: [AudioPlayerComponent, PlayerControlsComponent, PlayListComponent],
   imports: [
     CommonModule
   ],
