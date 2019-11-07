859b17364321590e1c597787032e3897cf6aae4f
mosquerapaul <mosquera.paul@gmail.com>
2019-11-07 09:15:16 +0100

    Folder structure and angular project created

---
+++ app.js
@@ -0,0 +1,22 @@
+/******************************************************************
+ *                          EXPRESS ROUTER
+ * Router realizado con Express
+ * Se encargará principalmente de devolver los archivos de audio 
+ * solicitados desde el reproductor.
+******************************************************************/
+
+
+/*          Definiciones
+***************************************/
+
+
+/*         Import de Controladores
+****************************************/
+
+
+/*         Resolución de rutas
+****************************************/
+
+/*         Rutas estáticas
+****************************************/
+
