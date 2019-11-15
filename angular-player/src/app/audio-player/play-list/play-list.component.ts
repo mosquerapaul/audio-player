cae22d8e883640a9dcd010205cce2310e2f7394d
mosquerapaul <mosquera.paul@gmail.com>
2019-11-15 22:55:52 +0100

    Add anim to play pause buttons and playlist

--- angular-player/src/app/audio-player/play-list/play-list.component.ts
+++ angular-player/src/app/audio-player/play-list/play-list.component.ts
@@ -2,9 +2,30 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PlayerStateService, PlayerState } from '../services/player-state.service';
 import { Observable, Subscription } from 'rxjs';
 import { AudioElement, PlayListService } from '../services/play-list.service';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-play-list',
+  animations: [
+    trigger('show-hide', [
+      transition(':enter', [
+        style({
+          opacity: 0
+        }),
+        animate('0.5s 2s', style({
+          opacity: 1
+        }))
+      ]),
+      transition(':leave', [
+        style({
+          opacity: 1
+        }),
+        animate('0.5s 1s', style({
+          opacity: 0
+        }))
+      ]),
+    ]),
+  ],
   templateUrl: './play-list.component.html',
   styleUrls: ['./play-list.component.scss']
 })
