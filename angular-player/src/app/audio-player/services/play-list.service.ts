b33d4f2f305c47fdda7b5c48b1ce966ba3d59dc5
mosquerapaul <mosquera.paul@gmail.com>
2019-11-19 10:47:22 +0100

    Playing info size and color Controls size and margins

--- angular-player/src/app/audio-player/services/play-list.service.ts
+++ angular-player/src/app/audio-player/services/play-list.service.ts
@@ -6,18 +6,57 @@ export interface AudioElement {
   sourceURL: string;
   duration: number;
   audioTitle: string;
+  artist: string;
 }
 
 const staticPlayList: AudioElement[] = [
   {
-    sourceURL: './assets/02 Shoot to Thrill.mp3',
-    duration: 156,
-    audioTitle: 'Shoot to Thrill'
+    sourceURL: './assets/bensound-allthat.mp3',
+    duration: 146000,
+    audioTitle: 'All that',
+    artist: 'Benjamin Tissot'
   },
   {
-    sourceURL: './assets/03 Back in Black.mp3',
-    duration: 230,
-    audioTitle: 'Back in Black'
+    sourceURL: './assets/bensound-beyondtheline.mp3',
+    duration: 186000,
+    audioTitle: 'Beyond the line',
+    artist: 'Benjamin Tissot'
+  },
+  {
+    sourceURL: './assets/bensound-happyrock.mp3',
+    duration: 105000,
+    audioTitle: 'Happy rock',
+    artist: 'Benjamin Tissot'
+  },
+  {
+    sourceURL: './assets/bensound-highoctane.mp3',
+    duration: 155000,
+    audioTitle: 'High Octane',
+    artist: 'Benjamin Tissot'
+  },
+  {
+    sourceURL: './assets/bensound-hipjazz.mp3',
+    duration: 164000,
+    audioTitle: 'Hip Jazz',
+    artist: 'Benjamin Tissot'
+  },
+  {
+    sourceURL: './assets/bensound-jazzcomedy.mp3',
+    duration: 193000,
+    audioTitle: 'Jazz Comedy',
+    artist: 'Benjamin Tissot'
+  },
+  {
+    sourceURL: './assets/bensound-punky.mp3',
+    duration: 126000,
+    audioTitle: 'Punky',
+    artist: 'Benjamin Tissot'
+  },
+  {
+    sourceURL: './assets/bensound-rumble.mp3',
+    duration: 154000,
+    audioTitle: 'Rumble',
+    artist: 'Benjamin Tissot'
   }
 ];
 
