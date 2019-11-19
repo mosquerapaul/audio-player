c8389e366395a20c6df9c7fd83d9ff60eb9e63c2
mosquerapaul <mosquera.paul@gmail.com>
2019-11-19 18:10:42 +0100

    basic express server: - Serves files at public folder as static - Redirects any url to / - Normalizes port to Env.Port or 3000

--- app.js
+++ app.js
@@ -5,6 +5,18 @@
  * solicitados desde el reproductor.
 ******************************************************************/
 
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const normalizePort = require('normalize-port');
+
+const port = normalizePort(process.env.PORT || '3000');
+
+/***********************************************
+ *  ENCODING PARAMETERS
+************************************************/
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 /*          Definiciones
 ***************************************/
@@ -17,6 +29,11 @@
 /*         Resolución de rutas
 ****************************************/
 
+
 /*         Rutas estáticas
 ****************************************/
+app.use(['/public', '/'], express.static('public'));
+app.use('*', (req, res) => res.redirect('/'));
 
+console.log(`Server started at port ${port} look at url: localhost:${port}`);
+app.listen(port);
