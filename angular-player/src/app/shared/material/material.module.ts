2fa9e9a7e2d49eb94bbeb3594d46d7c252290748
mosquerapaul <mosquera.paul@gmail.com>
2019-12-07 18:56:54 +0100

    Correcciones de diseño: - Añadida clase material background al body - cierre de div que faltaba en playlist - Añadido elemento material side-nav

--- angular-player/src/app/shared/material/material.module.ts
+++ angular-player/src/app/shared/material/material.module.ts
@@ -3,15 +3,15 @@ import { CommonModule } from '@angular/common';
 import { MatNativeDateModule, MatSnackBarModule, MatIconModule, MatDialogModule, MatButtonModule,
   MatTableModule, MatPaginatorModule , MatSortModule, MatTabsModule, MatCheckboxModule, MatToolbarModule,
   MatCard, MatCardModule, MatFormField, MatFormFieldModule, MatProgressSpinnerModule, MatInputModule,
-  MatSidenavModule } from '@angular/material';
+  MatSidenavModule, MatTooltipModule } from '@angular/material';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 
 
@@ -24,13 +24,13 @@ import {MatProgressBarModule} from '@angular/material/progress-bar';
     MatDialogModule, MatProgressSpinnerModule, MatButtonModule, MatSortModule, MatTableModule,
     MatTabsModule, MatCheckboxModule, MatToolbarModule, MatCardModule, MatFormFieldModule,
     MatProgressSpinnerModule, MatInputModule, MatPaginatorModule, MatListModule, MatGridListModule,
-    MatProgressBarModule, MatSidenavModule],
+    MatProgressBarModule, MatSidenavModule, MatTooltipModule],
 
   exports: [MatTabsModule, MatDividerModule, MatSliderModule, MatSelectModule, MatRadioModule,
     MatNativeDateModule, MatDatepickerModule, MatSnackBarModule, MatIconModule, MatDialogModule,
     MatProgressSpinnerModule, MatButtonModule, MatSortModule, MatCheckboxModule, MatToolbarModule,
     MatCardModule, MatTableModule, MatTabsModule, MatFormFieldModule, MatProgressSpinnerModule,
     MatInputModule, MatPaginatorModule, MatListModule, MatGridListModule,
-    MatProgressBarModule, MatSidenavModule ]
+    MatProgressBarModule, MatSidenavModule, MatTooltipModule ]
 })
 export class MaterialModule { }
